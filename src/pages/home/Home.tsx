e49f232ac5da8e71eab829ecefea17992ed47405
Alexey <dubrik90@yandex.by>
2023-06-02 20:27:49 +0300

    add slider

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -17,7 +17,6 @@ export const Home = () => {
       <JetChillerAbout />
       <OfficialSupplier />
       <JetDelivery />
-      {/*<JetSliderReviews/>*/}
       <SliderComp />
     </HomeWrapper>
   )
